66c46a9e661c8ff57b59b76d9be4458fadc3d2fe
GaryChan029 <garychan929@ymail.com>
2019-03-31 16:33:08 +0800

    1.0

--- multimap.cpp
+++ multimap.cpp
@@ -8,10 +8,13 @@
 using namespace std;
 
 int main(){
-	string key = "";//启动词
+	//启动词
+	string key="春天";
 	string passage=key,learnData,check,check2;
-	srand(int(time(0))); //随机种子 
-	multimap<string,string> wordData;//字词库 
+	//随机种子 
+	srand(int(time(0)));
+	//使用multimap作为字词库的载体 
+	multimap<string,string> wordData; 
 	typedef multimap<string,string>::iterator wordItor;
 	string keyTmp,wordTmp; 
 	ifstream fin;
@@ -19,7 +22,7 @@ int main(){
 	while(fin>>keyTmp>>wordTmp){
 		wordData.insert(pair<string,string>(keyTmp,wordTmp));
 	} 
-	fin.close();
+	fin.close();。 
 	cout<<"******************************************"<<endl;
 	cout<<"欢迎使用自动文章/句子生成器！             "<<endl;
 	cout<<"请输入两个中文汉字作为启动字！            "<<endl;
@@ -34,34 +37,45 @@ int main(){
 			passage+=key;
 			break;
 		}
-	}*/
+	}*/ 
     wordItor upper,lower;
     int cntRand=0;
-    //****开始生成句子 **** 
+    //开始生成句子（后测循环） 
     do{
+    	//计算有多少个对应的value 
     	int cntKey=wordData.count(key);
+    	//寻找对应的value(第一个地址) 
    		lower=wordData.find(key);
         while(1){
+        	//如果值为真的话，则把value拼接到文章上 
         	if(rand()%2){
         		passage+=(*lower).second;
         		cntRand=0;
         		break;
         	}
-        	lower++;
-        	if(++cntRand==cntKey){
-        		lower=wordData.find(key);
-        		cntRand=0;
-        	} 
+        	//随机一个value 
+			for(int i=0;i<rand()%100;i++){
+				lower++;
+        		if(++cntRand==cntKey){
+        			lower=wordData.find(key);
+        			cntRand=0;
+        		} 
+			} 
         }
+        //key=文章的最后两个字 
    		key=passage.substr(passage.length()-4,passage.length()-1);
-   		check=key.substr(2,2);
-   		/* 结束/标点符号判断器 */ 
-		if(check=="。"||check=="，"){
+   		//检查key的后一个中文字 
+   		check=key.substr(2,2); 
+   		//标点符号判断器
+		if(check=="。"||check=="，"||check=="！"||check=="？"||check=="："||check=="；"||check=="、"){
+			//随机结束
 			if(rand()%2&&check=="。")break;
-			key=check; 
-		}	
+			key=check;
+		}
+		//当找不到对应的key的时候，结束
    		if(wordData.find(key)==wordData.end())break;
     }while(1);
+    
     int cntText=0;
 	for(int i=0;i<passage.length();i++){
 		cout<<passage[i];
@@ -72,5 +86,6 @@ int main(){
 		} 
 	}
 	cout<<endl;
+	system("pause");
 	return 0; 
 } 
--- 字词库生产程序.cpp
+++ 字词库生产程序.cpp
@@ -9,37 +9,73 @@ using namespace std;
 
 int main(){
 	ifstream fin;
-	string keyTmp,wordTmp,check,check2,learnData; 
+	//依次为 临时key，临时字，检查器1，检查器2，要学习的数据 
+	string keyTmp,wordTmp,check,learnData; 
 	char file[200];
-	cout<<"请输入要变成字词库的文章的路径:" ;
+	cout<<"请输入要变成字词库的文章的路径（文章不能含有任何的英文字符！）:" ;
 	cin>>file;
+	//使用multimap作为字词库的载体 
 	multimap<string,string> wordData;
+	//设置一个迭代器 
 	typedef multimap<string,string>::iterator wordItor;
 	fin.open(file);
 	while(fin>>learnData){ 
 	for(int i=0;i<learnData.length()-4;i+=2){
-	 		check=learnData.substr(i,2);
-			check2=learnData.substr(i+2,2);
-			if(check2=="。"||check2=="，")continue; 
-			if(check=="。"||check=="，"){
-				keyTmp=learnData.substr(i,2);
-				wordTmp=learnData.substr(i+2,4);
+			//检测器，截取一个字节检查是否为英语标点符号
+			check=learnData.substr(i,1);
+			//遇到一切英文标点符号，一律变成句号
+			if(ispunct(check[0])){
+				keyTmp="。" ;
+				i++;
+				wordTmp=learnData.substr(i,4);
+				wordData.insert(pair<string,string>(keyTmp,wordTmp));
+				continue; 
 			}
+			//检测器 ，截取一个中文字符后的一个字节是否为英语标点符号，如果是，直接跳过
+			check=learnData.substr(i+2,1);
+			if(ispunct(check[0]))continue; 
+			//检查器，截取第二个中文字符 
+	 		check=learnData.substr(i+2,2);
+	 		//如果检查器为标点符号，跳过 
+	 		if(check=="。"||check=="，"||check=="！"||check=="？"||check=="："||check=="；"||check=="、")continue;
+	 		//检测器，截取第一个中文字符 
+			check=learnData.substr(i,2);
+			//如果检查为标点符号，则标点符号作为key 
+			if(check=="。"||check=="，"||check=="！"||check=="？"||check=="："||check=="；"||check=="、"){
+				keyTmp=learnData.substr(i,2);
+				//检查后一个字节是否为英文字符，是的话默认为句号 
+				check=learnData.substr(i+2,1);
+				if(ispunct(check[0])){
+					wordTmp="。" ;
+				}
+				else{
+					wordTmp=learnData.substr(i+2,4);
+				}
+			}
+			//否则默认前两个中文字符作为key 
 			else{
 				keyTmp=learnData.substr(i,4);
-				wordTmp=learnData.substr(i+4,2);
+				//检查后一个字节是否为英文字符，是的话默认为句号 
+				check=learnData.substr(i+4,1);
+				if(ispunct(check[0])){
+					wordTmp="。" ;
+				}
+				else{
+					wordTmp=learnData.substr(i+4,2);
+				}
 			}
 			wordData.insert(pair<string,string>(keyTmp,wordTmp));
 		}
 	}
-	fin.close();
+	fin.close(); 
 	ofstream fout;
 	fout.open("wordData.txt",ios::app);
+	//开始追加字词到字词库 
 	for(wordItor i=wordData.begin();i!=wordData.end();i++)
  	{
-  		fout<<(*i).first<<" "
-  		 <<(*i).second<<endl;
+  		fout<<(*i).first<<" "<<(*i).second<<endl;
  	}
 	fout.close();
+	system("pause");
 	return 0;	
 }
